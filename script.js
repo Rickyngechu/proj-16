b85c7a80f4d3216debdd67d69721f4d37c1c211e
Rickyngechu <ngerick00@gmail.com>
2024-02-06 02:13:48 +0300

    Form functionality up and running

--- script.js
+++ script.js
@@ -1,41 +1,122 @@
-// "use strict";
-// const inputCheck = document.querySelector(".toggle-inpt");
-// const spanMonth = document.querySelector(".month");
-// const spanYear = document.querySelector(".year");
-// const toggleItem = document.querySelector(".toggle-bg-item");
+"use strict";
+const inputCheck = document.querySelector(".toggle-inpt");
+const spanMonth = document.querySelector(".month");
+const spanYear = document.querySelector(".year");
+const toggleItem = document.querySelector(".toggle-bg-item");
 
-// const priceT = document.querySelector(".check-pricet");
-// const priceTime = document.querySelectorAll(".price-time");
-// const priceNum = document.querySelectorAll(".price-num");
+const priceT = document.querySelector(".check-pricet");
+const priceTime = document.querySelectorAll(".price-time");
+const priceNum = document.querySelectorAll(".price-num");
 
-// const yearPlusTag = document.querySelectorAll(".year-tplus");
-// // Converting the prices automatically
-// const yearMonthPrice = function (price, month = false) {
-//   let p;
-//   month === true
-//     ? (p = +price.innerHTML.split("").slice(1).join("") * 10)
-//     : (p = +price.innerHTML.split("").slice(1).join("") / 10);
+const yearPlusTag = document.querySelectorAll(".year-tplus");
+// Converting the prices automatically
+const yearMonthPrice = function (price, month = false) {
+  let p;
+  month === true
+    ? (p = +price.innerHTML.split("").slice(1).join("") * 10)
+    : (p = +price.innerHTML.split("").slice(1).join("") / 10);
 
-//   price.innerHTML = `$${p}`;
-// };
+  price.innerHTML = `$${p}`;
+};
 
-// inputCheck.addEventListener("click", function (e) {
-//   if (e.target.checked === true) {
-//     spanYear.classList.add("selected-time");
-//     spanMonth.classList.remove("selected-time");
+inputCheck.addEventListener("click", function (e) {
+  if (e.target.checked === true) {
+    spanYear.classList.add("selected-time");
+    spanMonth.classList.remove("selected-time");
 
-//     yearPlusTag.forEach(el => (el.style.display = "block"));
-//     priceTime.forEach(time => (time.innerHTML = "yr"));
-//     priceNum.forEach(price => yearMonthPrice(price, true));
+    yearPlusTag.forEach(el => (el.style.display = "block"));
+    priceTime.forEach(time => (time.innerHTML = "yr"));
+    priceNum.forEach(price => yearMonthPrice(price, true));
 
-//     priceTime.forEach(el => el.insertAdjacentHTML("afterbegin", html));
-//   } else {
-//     spanYear.classList.remove("selected-time");
-//     spanMonth.classList.add("selected-time");
+    priceTime.forEach(el => el.insertAdjacentHTML("afterbegin", html));
+  } else {
+    spanYear.classList.remove("selected-time");
+    spanMonth.classList.add("selected-time");
 
-//     priceTime.forEach(time => (time.innerHTML = "mon"));
-//     priceNum.forEach(price => yearMonthPrice(price, false));
+    priceTime.forEach(time => (time.innerHTML = "mon"));
+    priceNum.forEach(price => yearMonthPrice(price, false));
 
-//     yearPlusTag.forEach(el => (el.style.display = "none"));
-//   }
-// });
+    yearPlusTag.forEach(el => (el.style.display = "none"));
+  }
+});
+
+const slider = function () {
+  const slides = document.querySelectorAll(".slide");
+  const btnLeft = document.querySelector(".slider__btn--left");
+  const btnRight = document.querySelector(".slider__btn--right");
+  const dotContainer = document.querySelector(".dots");
+  const dots = document.querySelectorAll(".dot");
+
+  let curSlide = 0;
+  const maxSlide = slides.length;
+
+  // Functions
+  // const createDots = function () {
+  //   slides.forEach(function (_, i) {
+  //     dotContainer.insertAdjacentHTML(
+  //       "beforeend",
+  //       `<button class="dots__dot" data-slide="${i}"></button>`
+  //     );
+  //   });
+  // };
+
+  const activateDot = function (slide) {
+    document
+      .querySelector(`.dot[data-slide="${slide}"]`)
+      .classList.add("dots__dot--active");
+  };
+
+  const goToSlide = function (slide) {
+    slides.forEach(
+      (s, i) => (s.style.transform = `translateX(${100 * (i - slide)}%)`)
+    );
+  };
+
+  // Next slide
+  const nextSlide = function () {
+    if (curSlide === maxSlide - 1) {
+      curSlide = 0;
+    } else {
+      curSlide++;
+    }
+
+    goToSlide(curSlide);
+    // activateDot(curSlide);
+  };
+
+  const prevSlide = function () {
+    if (curSlide === 0) {
+      curSlide = maxSlide - 1;
+    } else {
+      curSlide--;
+    }
+    goToSlide(curSlide);
+    // activateDot(curSlide);
+  };
+
+  const init = function () {
+    goToSlide(0);
+    // createDots();
+
+    // activateDot(0);
+  };
+  init();
+
+  // Event handlers
+  btnRight.addEventListener("click", nextSlide);
+  btnLeft.addEventListener("click", prevSlide);
+
+  document.addEventListener("keydown", function (e) {
+    if (e.key === "ArrowLeft") prevSlide();
+    e.key === "ArrowRight" && nextSlide();
+  });
+
+  // dotContainer.addEventListener("click", function (e) {
+  //   if (e.target.classList.contains("dots__dot")) {
+  //     const { slide } = e.target.dataset;
+  //     goToSlide(slide);
+  //     // activateDot(slide);
+  //   }
+  // });
+};
+slider();
